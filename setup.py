7e2505ec42d1355b4a763fbd10e9a76dace04919
mercuree <mercuree.lab@gmail.com>
2017-02-09 18:27:12 +0200

    version bump

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with codecs.open('README.rst', 'r', 'utf-8') as f:
 
 setup(
     name='newspaper3k',
-    version='0.1.9.1',
+    version='0.1.9.11',
     description='Simplified python article discovery & extraction.',
     long_description=readme,
     author='Lucas Ou-Yang',
