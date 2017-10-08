96aff42b1dd9d01e5080a4a1adb7e8826da52e01
mercuree <mercuree.lab@gmail.com>
2017-10-08 14:32:40 +0300

    remove whitespaces a little faster

--- newspaper/text.py
+++ newspaper/text.py
@@ -13,14 +13,11 @@ import string
 
 from .utils import FileHelper
 
-TABSSPACE = re.compile(r'[\s\t]+')
-
 
 def innerTrim(value):
     if isinstance(value, str):
         # remove tab and white space
-        value = re.sub(TABSSPACE, ' ', value)
-        value = ''.join(value.splitlines())
+        value = ' '.join(value.split())
         return value.strip()
     return ''
 
